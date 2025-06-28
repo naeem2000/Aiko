41294c29800936eae4552a74d3b07ec169ac977d
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2025-06-28 21:00:11 +0200

    feat(reservation): finished reservation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Reservation from '@/stories/Reservation';
 import Gallery from './home/Gallery';
 import About from './home/About';
 import Hero from './home/Hero';
@@ -8,6 +9,7 @@ export default function Home() {
 			<Hero />
 			<About />
 			<Gallery />
+			<Reservation />
 		</main>
 	);
 }
--- src/modules/types.ts
+++ src/modules/types.ts
@@ -2,3 +2,15 @@ export type NavLinks = {
 	link: string;
 	url: string;
 }[];
+
+export type Menu = {
+	item: string;
+	description: string;
+	price: string;
+}[];
+
+export type Reservation = {
+	url: string;
+	image: string;
+	buttonText: string;
+}[];
