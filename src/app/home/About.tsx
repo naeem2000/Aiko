e8b2234b5e401ea75fe500576ca278b718e8aa05
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2025-06-28 16:45:51 +0200

    feat(gallery): finished gallery

--- src/app/home/About.tsx
+++ src/app/home/About.tsx
@@ -1,4 +1,5 @@
 import TextCard from '@/stories/TextCard';
+import Image from 'next/image';
 import React from 'react';
 
 export default function About() {
@@ -10,13 +11,28 @@ export default function About() {
 			<h2 className='text-center font-medium text-6xl leading-[100%] tracking-[6%] mt-5'>
 				ABOUT US
 			</h2>
-			<div>
+			<div className='flex justify-center items-center gap-12 flex-wrap mt-14'>
+				<Image
+					src={'/images/about-1.png'}
+					alt='Sushi platter'
+					width={381}
+					height={478}
+					className='object-contain'
+				/>
 				<TextCard
 					variant='dark'
 					heading='Taste Preparation'
 					subHeading='TRADITIONAL & MODERN'
 					text='Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.'
 				/>
+				<div>
+					<Image
+						src={'/images/about-2.png'}
+						alt='Sushi platter'
+						width={394}
+						height={478}
+					/>
+				</div>
 			</div>
 		</section>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from './home/About';
+import Gallery from './home/Gallery';
 import Hero from './home/Hero';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
 		<main>
 			<Hero />
 			<About />
+			<Gallery />
 		</main>
 	);
 }
