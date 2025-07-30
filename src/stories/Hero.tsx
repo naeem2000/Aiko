de0ae413c7a93c9a9676c91a7b3f7d895068ae50
Naeem Carr <73929659+naeem2000@users.noreply.github.com>
2025-07-30 17:26:24 +0200

    fix(): added copy to hero

--- src/stories/Hero.tsx
+++ src/stories/Hero.tsx
@@ -15,13 +15,27 @@ export default function Hero({ bg }: Props) {
 		>
 			<div className='absolute right-0 left-0 bottom-0 top-0 bg-[rgb(0,0,0,0.5)] z-10' />
 			{path === '/' && (
-				<div className='flex items-center justify-center flex-col h-full w-full z-20 relative'>
+				<div className='flex items-center justify-center flex-col h-full w-full z-20 relative gap-3'>
 					<h2 className='font-[275] text-2xl xl:text-3xl leading-[100%] tracking-[3%] text-white'>
 						Refined sushi. Elevated taste
 					</h2>
 					<h1 className="max-w-[629px] font-['Carattere'] text-6xl xl:text-[92.18px] leading-[100%] tracking-[6%] text-center mt-8 text-white">
-						Always Aiko
+						The Aiko Experience
 					</h1>
+					<h3 className='font-bolder text-2xl xl:text-2xl leading-[100%] tracking-[3%] text-white'>
+						Expertly crafted at all 3 of our signature locations
+					</h3>
+					<p className='font-[275] text-2xl xl:text-xl leading-[100%] tracking-[3%] text-white'>
+						<strong>Aiko on bree: </strong>Aiko orginal sushi.
+					</p>
+					<p className='font-[275] text-2xl xl:text-xl leading-[100%] tracking-[3%] text-white'>
+						<strong>Aiko Riverlands:</strong> Aikos ramen and signatures crafted
+						to excellence.
+					</p>
+					<p className='font-[275] text-2xl xl:text-xl leading-[100%] tracking-[3%] text-white'>
+						<strong>Aiko Plus, 70 Bree Street:</strong> Sushi meets art on a
+						plate.
+					</p>
 				</div>
 			)}
 		</section>
